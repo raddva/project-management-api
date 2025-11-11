dafbfbf1f1c68a6cf4bd5e3c269fad325071a1a5
raddva <111165508+raddva@users.noreply.github.com>
2025-11-11 09:46:08 +0700

    feat: Create Board #3

--- controllers/board_controller.go
+++ controllers/board_controller.go
@@ -2,6 +2,8 @@ package controllers
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt/v4"
+	"github.com/google/uuid"
 	"github.com/raddva/projeqtor-api-go/models"
 	"github.com/raddva/projeqtor-api-go/services"
 	"github.com/raddva/projeqtor-api-go/utils"
@@ -16,11 +18,22 @@ func NewBoardController(s services.BoardService) *BoardController {
 }
 
 func (c *BoardController) CreateBoard(ctx *fiber.Ctx) error {
+	var userID uuid.UUID
+	var err error
 	board := new(models.Board)
+	user := ctx.Locals("user").(*jwt.Token)
+	claims := user.Claims.(jwt.MapClaims)
 
 	if err := ctx.BodyParser(board); err != nil {
 		return utils.BadRequest(ctx, "Failed to read Request", err.Error())
 	}
+	
+	userID, err = uuid.Parse(claims["pub_id"].(string))
+	if err != nil {
+		return utils.BadRequest(ctx, "Failed Processing Request", err.Error())
+		
+	}
+	board.OwnerPublicID = userID
 
 	if err := c.service.Create(board); err != nil {
 		return utils.BadRequest(ctx, "Failed Saving Data", err.Error())
